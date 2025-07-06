c716cf200797b327356f050ad7a64dad85b09996
Stephen Stephenson <stephenson.inbox@gmail.com>
2025-07-06 17:23:19 +0200

    Improve yt-dlp script

--- yt-dlp/yt-dlp-audio-only.sh
+++ yt-dlp/yt-dlp-audio-only.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-yt-dlp -i --extract-audio --audio-format mp3 --audio-quality 0 --output "%(uploader)s - %(upload_date>%Y-%m-%d)s - %(title)s [%(id)s].%(ext)s" -- "$@"
+yt-dlp -i \
+  --extract-audio \
+  --audio-format mp3 \
+  --audio-quality 0 \
+  --embed-metadata \
+  --embed-thumbnail \
+  --retries 100 \
+  --min-sleep-interval 3 \
+  --max-sleep-interval 10 \
+  --output "%(uploader)s - %(upload_date>%Y-%m-%d)s - %(title)s [%(id)s].%(ext)s" \
+  -- "$@"
