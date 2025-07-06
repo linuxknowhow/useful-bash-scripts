6a8aaf08a420ac68ed671d305382447405725151
Stephen Stephenson <stephenson.inbox@gmail.com>
2025-07-06 20:23:42 +0200

    Improve yt-dlp scripts

--- yt-dlp/yt-dlp-all-formats.sh
+++ yt-dlp/yt-dlp-all-formats.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-yt-dlp -f 'bestvideo[height=480]+bestaudio[ext=m4a],bestvideo[height=720]+bestaudio[ext=m4a],bestvideo[height=1080]+bestaudio[ext=m4a],bestvideo[height=1440]+bestaudio[ext=m4a],bestvideo[height=2160]+bestaudio[ext=m4a],bestvideo[height=4320]+bestaudio[ext=m4a],bestvideo+bestaudio[ext=m4a]/best[ext=mp4]/best' --merge-output-format mp4 --output "%(uploader)s - %(upload_date>%Y-%m-%d)s - %(title)s [%(id)s] (%(resolution)s).%(ext)s" -- "$@"
+yt-dlp -f 'bestvideo[height=480]+bestaudio[ext=m4a],
+           bestvideo[height=720]+bestaudio[ext=m4a],
+           bestvideo[height=1080]+bestaudio[ext=m4a],
+           bestvideo[height=1440]+bestaudio[ext=m4a],
+           bestvideo[height=2160]+bestaudio[ext=m4a],
+           bestvideo[height=4320]+bestaudio[ext=m4a],
+           bestvideo+bestaudio[ext=m4a]/best[ext=mp4]/best' \
+  --merge-output-format mp4 \
+  --embed-metadata \
+  --embed-thumbnail \
+  --retries 100 \
+  --min-sleep-interval 3 \
+  --max-sleep-interval 10 \
+  --output "%(uploader)s - %(upload_date>%Y-%m-%d)s - %(title)s [%(id)s] (%(resolution)s).%(ext)s" \
+  -- "$@"
--- yt-dlp/yt-dlp-twitch.sh
+++ yt-dlp/yt-dlp-twitch.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-yt-dlp --output "%(uploader)s - %(upload_date>%Y-%m-%d)s - %(title)s [%(id)s].%(ext)s" -- "$@"
+yt-dlp \
+  -f 'best[height>=720]' \
+  --concurrent-fragments 5 \
+  --cookies-from-browser firefox \
+  --embed-thumbnail \
+  --embed-metadata \
+  --retries 100 \
+  --min-sleep-interval 3 \
+  --max-sleep-interval 10 \
+  --output "%(uploader)s - %(upload_date>%Y-%m-%d)s - %(title)s [%(id)s].%(ext)s" \
+  -- "$@"
