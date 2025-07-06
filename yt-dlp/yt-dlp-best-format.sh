809540e6c7af6fb77ec63e31a80e5f56b0123c30
Stephen Stephenson <stephenson.inbox@gmail.com>
2025-07-06 11:17:52 +0200

    Improve yt-dlp script

--- yt-dlp/yt-dlp-best-format.sh
+++ yt-dlp/yt-dlp-best-format.sh
@@ -4,6 +4,9 @@ yt-dlp -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio/best[ext=mp
   --merge-output-format mp4 \
   --embed-metadata \
   --embed-thumbnail \
-  --retries 10 \
+  --retries 100 \
+  --min-sleep-interval 3 \
+  --max-sleep-interval 10 \
+  --cookies-from-browser firefox \
   --output "%(uploader)s - %(upload_date>%Y-%m-%d)s - %(title)s [%(id)s].%(ext)s" \
   -- "$@"
